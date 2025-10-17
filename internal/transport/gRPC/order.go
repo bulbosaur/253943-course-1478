88e957d5fd6c84a61c58e2cdd684929dc810f301
Sofiya <sofiytula71@gmail.com>
2025-10-17 17:44:39 +0300

    working on the grpc server

--- internal/transport/gRPC/order.go
+++ internal/transport/gRPC/order.go
@@ -1,5 +1,19 @@
 package v1
 
-// type orderServiceServer struct {
-// 	pb.UnimplementedOrderServiceServer
-// }
+import (
+	storage "gitlab.crja72.ru/golang/2025/spring/course/students/253943-Sofiytula71-gmail.com-course-1478/-/tree/main"
+	pb "gitlab.crja72.ru/golang/2025/spring/course/students/253943-Sofiytula71-gmail.com-course-1478/-/tree/main/pkg/api/test"
+)
+type OrderServiceServer struct {
+	pb.UnimplementedOrderServiceServer
+	storage *storage.OrderStorage
+}
+
+func NewOrderServiceServer() *OrderServiceServer {
+	return &OrderServiceServer{
+		pb.UnimplementedOrderServiceServer{},
+		storage.NewOrderStorage(),
+	}
+}
+
+func (s *OrderServiceServer) CreateOrder()
