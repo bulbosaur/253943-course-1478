86ccf7e9703c8c1106bcba2fee9f602c760b8a0d
Sofiya <sofiytula71@gmail.com>
2025-11-18 14:48:58 +0300

    add dlq

---
+++ pkg/faulttolerance/dlq.go
@@ -0,0 +1,26 @@
+package faulttolerance
+
+type DeadLetterQueue struct {
+	messages []string
+}
+
+func NewDeadLetterQueue() *DeadLetterQueue {
+	return &DeadLetterQueue{
+		messages: make([]string, 0),
+	}
+}
+
+func (s *DeadLetterQueue) GetMessages() []string {
+	return s.messages
+}
+
+func ProcessWithDLQ(messages []string, handler func(msg string) error, dlq DeadLetterQueue) {
+	var err error
+
+	for _, msg := range messages {
+		err = handler(msg)
+		if err != nil {
+			dlq.messages = append(dlq.messages, msg)
+		}
+	}
+}
